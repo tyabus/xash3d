c6b2f8e78833716617023a6716b3266c2758f772
tyabus <tyabustest@gmail.com>
2020-10-23 22:48:07 +0600

    Try to fix android build

--- scripts/build_android_engine.sh
+++ scripts/build_android_engine.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/xash3d-android-project
-sh gen-version.sh travis build
-sh gen-config.sh test
+sh android/gen-version.sh travis build
+sh android/gen-config.sh test
 python2 scripts/makepak.py xash-extras android/assets/extras.pak
 ndk-build -j2 APP_CFLAGS="-w" NDK_CCACHE=ccache
-ant debug -Dant.build.javac.target=1.6 -Dant.build.javac.source=1.6 -Dtest.version=1
+ant debug -Dtest.version=1
 cp bin/xashdroid-debug.apk $TRAVIS_BUILD_DIR/xash3d-generic.apk
 tar cJf $TRAVIS_BUILD_DIR/android-debug-symbols.txz obj/local/*/*.so
