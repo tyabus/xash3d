ec05d6049444e23a0f6847bdbc3ee5b8a7741410
tyabus <tyabustest@gmail.com>
2020-10-24 01:13:53 +0600

    This should dot the job

--- scripts/build_android_engine.sh
+++ scripts/build_android_engine.sh
@@ -5,8 +5,7 @@ sh android/gen-version.sh travis build
 sh android/gen-config.sh test
 python2 scripts/makepak.py xash-extras android/assets/extras.pak
 ndk-build -j2 APP_CFLAGS="-w" NDK_CCACHE=ccache
-cd ./android/
+cd android
 ant debug -Dtest.version=1
-cd ../
 cp bin/xash3d-debug.apk $TRAVIS_BUILD_DIR/xash3d-generic.apk
 tar cJf $TRAVIS_BUILD_DIR/android-debug-symbols.txz obj/local/*/*.so
