ca3ef851feaa0b7359a7cd3c6279ddb7882ab5cc
tyabus <tyabustest@gmail.com>
2020-10-25 17:08:45 +0600

    Update paths in android build

--- scripts/build_android_engine.sh
+++ scripts/build_android_engine.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/xash3d-android-project
-sh android/gen-version.sh travis build
 sh android/gen-config.sh test
-python2 scripts/makepak.py xash-extras android/assets/extras.pak
+python2 scripts/makepak.py xash-extras assets/extras.pak
 ndk-build -j2 APP_CFLAGS="-w" NDK_CCACHE=ccache
 ln -s $TRAVIS_BUILD_DIR/xash3d-android-project/android/libs $TRAVIS_BUILD_DIR/xash3d-android-project/libs
 ant debug -Dtest.version=1
