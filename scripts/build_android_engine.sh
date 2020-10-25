4b1c449bebaf1586383b0d16183041e3df1fc70b
tyabus <tyabustest@gmail.com>
2020-10-25 15:12:04 +0600

    Fixed android build

--- scripts/build_android_engine.sh
+++ scripts/build_android_engine.sh
@@ -5,8 +5,6 @@ sh android/gen-version.sh travis build
 sh android/gen-config.sh test
 python2 scripts/makepak.py xash-extras android/assets/extras.pak
 ndk-build -j2 APP_CFLAGS="-w" NDK_CCACHE=ccache
-cd android
-mkdir -p libs
 ln -s $TRAVIS_BUILD_DIR/xash3d-android-project/android/libs $TRAVIS_BUILD_DIR/xash3d-android-project/libs
 ant debug -Dtest.version=1
 cp bin/xash3d-debug.apk $TRAVIS_BUILD_DIR/xash3d-generic.apk
