93c5425cca104363f164f1982f202b44c6221f85
tyabus <tyabustest@gmail.com>
2021-01-28 21:06:52 +0600

    Generate version string for android

--- scripts/build_android_engine.sh
+++ scripts/build_android_engine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/xash3d-android-project
-sh android/gen-config.sh test
+sh gen-version.sh
 python2 scripts/makepak.py xash-extras assets/extras.pak
 ndk-build -j2 APP_CFLAGS="-w" NDK_CCACHE=ccache
 ln -s $TRAVIS_BUILD_DIR/xash3d-android-project/android/libs $TRAVIS_BUILD_DIR/xash3d-android-project/libs
