e4bdfa50f52ee9bd06fbd1a455d5b984222237a9
tyabus <tyabustest@gmail.com>
2021-01-28 21:18:10 +0600

    Removed coverity scan variables from travis build scripts

--- scripts/travis_coverity_scan.sh
+++ scripts/travis_coverity_scan.sh
@@ -2,17 +2,10 @@
 
 cd $TRAVIS_BUILD_DIR
 
-#mkdir -p coverity && cd coverity/
-
 export CC="gcc"
 export CXX="g++"
 export CFLAGS="-m32"
 export CXXFLAGS="-m32"
-
-#export COVERITY_SCAN_BRANCH_PATTERN="coverity_scan"
-#export COVERITY_SCAN_NOTIFICATION_EMAIL="mr.maks0443@gmail.com"
-#export COVERITY_SCAN_PROJECT_NAME="tyabus-xash3d"
-#export COVERITY_SCAN_BUILD_COMMAND="make -j2 VERBOSE=1"
 
 cmake \
 	-DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sdl2-linux/usr/local \
