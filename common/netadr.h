d6f8fdfa66d44db5e866805f7eb62ff2ca7bb108
Mr0maks <mr.maks0443@gmail.com>
2021-05-08 18:20:21 +0500

    Replaced Valve header file with similar defines from Quake 2 qcommon/qcommon.h

--- common/netadr.h
+++ common/netadr.h
@@ -1,30 +1,29 @@
-/***
-*
-*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
-*
-*   Use, distribution, and modification of this source code and/or resulting
-*   object code is restricted to non-commercial enhancements to products from
-*   Valve LLC.  All other use, distribution, or modification is prohibited
-*   without written permission from Valve LLC.
-*
-****/
+/*
+This part from Quake 2 qcommon/qcommon.h header file
+
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
 
 #ifndef NETADR_H
 #define NETADR_H
 
-typedef enum
-{
-	NA_UNUSED,
-	NA_LOOPBACK,
-	NA_BROADCAST,
-	NA_IP,
-	NA_IPX,
-	NA_BROADCAST_IPX
-} netadrtype_t;
+typedef enum {NA_LOOPBACK = 1, NA_BROADCAST, NA_IP, NA_IPX, NA_BROADCAST_IPX} netadrtype_t;
 
 typedef struct netadr_s
 {
@@ -34,4 +33,4 @@ typedef struct netadr_s
 	unsigned short	port;
 } netadr_t;
 
-#endif//NETADR_H
+#endif // NETADR_H
