45329545b0ec64453c23224fdc49f1357bea4ba3
tyabus <tyabustest@gmail.com>
2021-06-09 21:02:50 +0600

    Don't register build cvar if game is tfc

--- engine/common/host.c
+++ engine/common/host.c
@@ -1238,12 +1238,14 @@ int EXPORT Host_Main( int argc, const char **argv, const char *progname, int bCh
 	host_limitlocal = Cvar_Get( "host_limitlocal", "0", 0, "apply cl_cmdrate and rate to loopback connection" );
 	con_gamemaps = Cvar_Get( "con_mapfilter", "1", CVAR_ARCHIVE, "when enabled, show only maps in game folder (no maps from base folder when running mod)" );
 	download_types = Cvar_Get( "download_types", "msec", CVAR_ARCHIVE, "list of types to download: Model, Sounds, Events, Custom" );
-	build = Cvar_Get( "build", "3366", CVAR_INIT, "left for compatibility reasons" );
 	ver = Cvar_Get( "ver", "48/0.98.3366", CVAR_INIT, "shows an original engine version. left for compability reasons" );
 	host_build = Cvar_Get( "host_build", va("%i", Q_buildnum() ), CVAR_INIT, "returns current build number" );
 	host_ver = Cvar_Get( "host_ver", va("%i %s %s %s %s", Q_buildnum(), XASH_VERSION, Q_buildos(), Q_buildarch(), Q_buildcommit() ), CVAR_INIT, "detailed info about this build" );
 	host_mapdesign_fatal = Cvar_Get( "host_mapdesign_fatal", "1", CVAR_ARCHIVE, "make map design errors fatal" );
 	host_xashds_hacks = Cvar_Get( "xashds_hacks", "0", 0, "hacks for xashds in singleplayer" );
+
+	if( Q_stricmp( GI->gamefolder, "tfc" ) )
+		build = Cvar_Get( "build", "3366", CVAR_INIT, "left for compatibility reasons" );
 
 	// content control
 	Cvar_Get( "violence_hgibs", "1", CVAR_ARCHIVE, "show human gib entities" );
