8a5e21a2a28e7486e601153ab3ca1beb1495308b
Mr0maks <mr.maks0443@gmail.com>
2022-08-18 20:02:18 +0500

    soundlib: fix CVE-2017-12839 in libmpg123
    Original patch: https://www.mpg123.de/cgi-bin/scm/mpg123/trunk/src/libmpg123/getbits.h?view=patch&r1=2024&r2=4323

--- engine/common/soundlib/libmpg/getbits.h
+++ engine/common/soundlib/libmpg/getbits.h
@@ -55,6 +55,10 @@ static uint32_t getbits( mpg123_handle_t *fr, int number_of_bits )
 {
 	ulong	rval;
 
+	if( (long)(fr->wordpointer-fr->bsbuf)*8
+		+ fr->bitindex+number_of_bits > (long)fr->framesize*8 )
+		return 0;
+
 	rval = fr->wordpointer[0];
 	rval <<= 8;
 	rval |= fr->wordpointer[1];
