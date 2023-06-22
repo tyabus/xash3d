3f42050e2c6783b5fa36d5d55d1afd27f63bed0b
tyabus <tyabustest@gmail.com>
2023-06-22 10:29:30 +0600

    engine: common: Mod_FreeUnused should NEVER try to free world model

--- engine/common/model.c
+++ engine/common/model.c
@@ -3082,7 +3082,8 @@ void Mod_FreeUnused( void )
 	model_t	*mod;
 	int	i;
 
-	for( i = 0, mod = cm_models; i < cm_nummodels; i++, mod++ )
+	// never tries to release worldmodel
+	for( i = 1, mod = cm_models; i < cm_nummodels; i++, mod++ )
 	{
 		if( !mod->name[0] ) continue;
 		if( mod->needload != world.load_sequence )
