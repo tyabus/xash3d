7a6a925e098165cd9a88621159c80f45f49aa49f
tyabus <tyabustest@gmail.com>
2024-11-21 08:50:48 +0500

    engine: common: Add missing value accessor for host_developer in Con_Printf

--- engine/common/common.c
+++ engine/common/common.c
@@ -490,7 +490,7 @@ void GAME_EXPORT Con_Printf( char *szFmt, ... )
 	static char	buffer[16384];	// must support > 1k messages
 	va_list		args;
 
-	if( host_developer <= 0 )
+	if( host_developer->value <= 0 )
 		return;
 
 	va_start( args, szFmt );
