tyabus/xash3d

commit 330e17b9d97498a188e40bcaab12a850a019c804

engine: common: Clean delta when getting delta from server (0.20 backport)

engine/common/net_encode.c
@@ -537,7 +537,8 @@ void Delta_ParseTableField( sizebuf_t *msg )
 		post_mul = BF_ReadFloat( msg );
 
 	// delta encoders it's already initialized on this machine (local game)
-	if( delta_init ) return;
+	if( delta_init )
+		Delta_Shutdown();
 
 	// add field to table
 	Delta_AddField( dt->pName, pName, flags, bits, mul, post_mul );

commit 5db7cece9807ecf2a664b5b9e329dddd3478ff0c

engine: common: Add delta field regardless if delta is initialized or not
Fixes regression introduced in 330e17b9d97498a188e40bcaab12a850a019c804

engine/common/net_encode.c
@@ -536,10 +536,6 @@ void Delta_ParseTableField( sizebuf_t *msg )
 	if( BF_ReadOneBit( msg ))
 		post_mul = BF_ReadFloat( msg );
 
-	// delta encoders it's already initialized on this machine (local game)
-	if( delta_init )
-		Delta_Shutdown();
-
 	// add field to table
 	Delta_AddField( dt->pName, pName, flags, bits, mul, post_mul );
 }